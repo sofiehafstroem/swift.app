ee609371e4b85c8360a78c189c7cefd20a184c25
Sofie Nielsen <sofiehafstroem@sofies-imac.local>
2016-09-14 12:10:00 +0200

    Initial Commit

---
+++ Swift - freakin iOS developers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift - freakin iOS developers
+//
+//  Created by Sofie Nielsen on 14/09/16.
+//  Copyright © 2016 Sofie Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
